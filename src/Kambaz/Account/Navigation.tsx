7c84513c3de6990a47339f4091446b7df0df3886
ruitingm <rmruitingma@gmail.com>
2025-02-05 20:04:42 -0800

    fixed bug in kambaz

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation">
-      {" "}
       <Link to={`/Kambaz/Account/Signin`}> Signin </Link> <br />{" "}
       <Link to={`/Kambaz/Account/Signup`}> Signup </Link> <br />{" "}
       <Link to={`/Kambaz/Account/Profile`}> Profile </Link> <br />{" "}
