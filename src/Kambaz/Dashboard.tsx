bad4ea2fd6cd98c23f699742f24c2b8ff39c54c2
ruitingm <rmruitingma@gmail.com>
2025-02-08 21:48:09 -0800

    up to 3.7.3

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,165 +1,39 @@
 import { Link } from "react-router-dom";
+import * as db from "./Database";
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashbaord-title">Dashboard</h1>
       <hr />
-      <h2 id="wd-dashboard-published">Published Courses (8)</h2>
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
       <div id="wd-dashboard-course" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/reactjs.png" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1234 React JS
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
+          {courses.map((course) => (
+            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+              <div className="card rounded-3 overflow-hidden">
+                <Link
+                  to="/Kambaz/Courses/${course._id}/Home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <img src={course.img} width="100%" height={160}></img>
+                  <div className="card-body">
+                    <h5 className="wd-dashbaord-course-title card-title">
+                      {course.name}
+                    </h5>
+                    <p
+                      className="wd-dashboard-course-title card-text overflow-hidden"
+                      style={{ maxHeight: "100px" }}
+                    >
+                      {course.description}
+                    </p>
+                    <button className="btn btn-primary"> Go </button>
+                  </div>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1235/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/python.jpeg" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1235 Python
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1236/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/C.jpg" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1236 C
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1237/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/C++.png" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1237 C++
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1238/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/java.jpg" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1238 Java
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1239/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/html.jpeg" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1239 HTML
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1240/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/ml.jpg" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1240 Machine Learning
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-            <div className="card rounded-3 overflow-hidden">
-              <Link
-                to="/Kambaz/Courses/1241/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <img src="/images/math.jpg" width="100%" height={160}></img>
-                <div className="card-body">
-                  <h5 className="wd-dashbaord-course-title card-title">
-                    CS1241 Discrete Math
-                  </h5>
-                  <p className="wd-dashboard-course-title card-text">
-                    Full Stack Software Developer
-                  </p>
-                  <button className="btn btn-primary"> Go </button>
-                </div>
-              </Link>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
